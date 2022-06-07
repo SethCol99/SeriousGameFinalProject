abfa6048884fab3d5bf37f48a4edf274f2058232
wastilla <wastilla@email.unc.edu>
2022-06-07 14:42:33 +0200

    Button Script

--- Assets/Scripts/TryAgainButtonScript.cs
+++ Assets/Scripts/TryAgainButtonScript.cs
@@ -7,27 +7,19 @@ using UnityEngine.UI;
 public class TryAgainButtonScript : MonoBehaviour
 {
 
-    public Driver target;
-
-    public Button resetButton;
+    
+    
 
     //// Start is called before the first frame update
     void Start()
     {
-        resetButton.enabled = false;
+        
     }
 
     //// Update is called once per frame
     void Update()
     {
-        if (target.playerDead)
-        {
-            resetButton.enabled = true;
-        }
-        else
-        {
-            resetButton.enabled = false;
-        }
+        
     }
 
     public void ResetGame()
